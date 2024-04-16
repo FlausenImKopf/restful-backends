5c1f2abecb932eeace30e16c3d65b7b1a9539217
FlausenImKopf <gluecksfeder@mailbox.org>
2024-04-16 09:10:06 +0200

    style: added styling; refactor:separated state and render

--- quote-app/script.js
+++ quote-app/script.js
@@ -1,26 +1,34 @@
 const main = document.getElementById("main");
 const btn = document.querySelector("button");
 
-function getQuotes() {
+let state = {};
+
+function getState() {
   const p = fetch("https://dummy-apis.netlify.app/api/quote");
-  p.then((response) => {
-    console.log(response.status);
-    console.log(response.ok);
-    return response.json();
-  }).then((data) => {
-    console.log(data);
-    const quote = document.getElementById("quote");
-    quote.textContent = data.quote;
-    const author = document.getElementById("author");
-    author.textContent = data.author;
-    main.append(quote, author);
-    // main.append(document.createTextNode(data.author));
-    // main.append(document.createTextNode(data.quote));
+  return p
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      state.quote = data.quote;
+      state.author = data.author;
+    });
+}
+
+function render() {
+  getState().then(() => {
+    const p1 = document.getElementById("quote");
+    p1.textContent = state.quote;
+    p1.className = "quote";
+    const p2 = document.getElementById("author");
+    p2.textContent = "-" + state.author;
+    p2.className = "author";
+    main.append(p1, p2);
   });
 }
 
-getQuotes();
+render();
 
 btn.addEventListener("click", () => {
-  getQuotes();
+  render();
 });
